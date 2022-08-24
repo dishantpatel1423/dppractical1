846cb93c6e69413979976cb986bcd80aa6a9ccdc
HarshChauhan18 <harshchauhan3974@gmail.com>
2022-08-24 10:06:22 +0530

    practical1_2

---
+++ src/main/kotlin/practical1_2.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+   var x : Int = 5
+   var y : Float = x.toFloat()
+
+   var a : String = "32"
+   var b : Int = a.toInt()
+
+   var c :String = "23"
+   var d : Float= c.toFloat()
+
+    println("flot:$y")
+    println("int:$b")
+    println("float:$c")
+
+}
