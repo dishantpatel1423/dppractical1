e2a0daa9ae12ee529095462a8ab7bd7c35fcc1c4
HarshChauhan18 <harshchauhan3974@gmail.com>
2022-08-24 10:31:08 +0530

    practical1_3

---
+++ src/main/kotlin/practical1_3.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+   println("student en: ")
+    var en = readLine()
+    println("student name: ")
+    var name = readLine()
+    println("student branch: ")
+    var branch = readLine()
+    println("student class: ")
+    var clas = readLine()
+    println("student batch: ")
+    var batch = readLine()
+    println("student collage name: ")
+    var collname = readLine()
+    println("student university: ")
+    var uniname = readLine()
+    println("student age: ")
+    var age = readLine()
+    println("student en:$en")
+    println("student name:$name")
+    println("student branch:$branch")
+    println("student class:$clas")
+    println("student batch:$batch")
+    println("student collname:$collname")
+    println("student uniname:$uniname")
+    println("student age:$age")
+
+
+
+}
